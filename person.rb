791f967dc98ce5f7dba2d7ced1b9339ba52ad3d7
eric <ericopokuantwi384@gmail.com>
2023-06-28 07:40:30 +0000

    finish OOP setup

--- person.rb
+++ person.rb
@@ -1,15 +1,14 @@
 class Person
-  attr_reader :id
-  attr_accessor :name, :age
-
-  def initialize(age, parent_permission: true, name = 'Unknown')
+  def initialize(age, parent_permission: true, name: 'Unknown')
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
+  attr_reader :id
+  attr_accessor :name, :age
 
-  def can_use_services
+  def can_use_services?
     @parent_permission || of_age?
   end
 
