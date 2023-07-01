0cdda0958870ee14636c485c95957922301298b1
eric <ericopokuantwi384@gmail.com>
2023-07-01 14:59:02 +0000

    finish oop UI

--- app.rb
+++ app.rb
@@ -5,9 +5,31 @@ require './student'
 require './teacher'
 
 class App
-  def initialize
+  def initialize(main)
+    @main = main
     @persons = []
     @books = []
+  end
+
+  def create_person
+    print 'Select [0] to create teacher or [1] to create student: '
+    type = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Age: '
+    age = gets.chomp.to_i
+    if type.zero?
+      print 'Specialization: '
+      specialization = gets.chomp
+      create_teacher(age, name, specialization)
+    elsif type == 1
+      print 'Has parent permission? [Y/N]: '
+      parent_permission = gets.chomp.capitalize
+      create_student(age, name, true) if parent_permission == 'Y'
+      create_student(age, name, false) if parent_permission == 'N'
+    end
+    puts 'Person created successfully!'
+    @main.show_options
   end
 
   def create_teacher(age, name, specialization)
@@ -20,12 +42,26 @@ class App
     @persons.push(student)
   end
 
-  def create_book(title, author)
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
     book = Book.new(title, author)
     @books.push(book)
+    puts 'Book created successfully'
+    @main.show_options
   end
 
-  def create_rental(date, book, person)
+  def create_rental
+    puts 'Select a book from the following list by number'
+    choose_book
+    book = gets.chomp.to_i
+    puts 'Select a person from the following list by number (not id)'
+    choose_person
+    person = gets.chomp.to_i
+    print 'Enter date (yyy/mm/dd): '
+    date = gets.chomp
     @persons.each do |p|
       @books.each do |b|
         next unless @persons.find_index(p) == person && @books.find_index(b) == book
@@ -33,8 +69,21 @@ class App
         rental = Rental.new(date, b, p)
         p.add_rental(rental)
         b.add_rental(rental)
-        puts 'book'
       end
+    end
+    puts 'Rental created successfully'
+    @main.show_options
+  end
+
+  def choose_book
+    @books.each do |book|
+      puts "#{@books.find_index(book)}) Title: #{book.title}, Author: #{book.author}"
+    end
+  end
+
+  def choose_person
+    @persons.each do |person|
+      puts "#{@persons.find_index(person)}) [#{person.type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
   end
 
@@ -42,19 +91,24 @@ class App
     @books.each do |book|
       puts "#{@books.find_index(book)}) Title: #{book.title}, Author: #{book.author}"
     end
+    @main.show_options
   end
 
   def list_people
     @persons.each do |person|
       puts "#{@persons.find_index(person)}) [#{person.type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
+    @main.show_options
   end
 
-  def list_rentals(id)
+  def list_rentals
+    print 'ID of person: '
+    id = gets.chomp.to_i
     @persons.each do |p|
       p.rentals.each do |rental|
         puts "Date: #{rental.date}, Book: \"#{rental.book.title}\", by: #{rental.book.author}" if p.id == id
       end
     end
+    @main.show_options
   end
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
-require './app'
+require_relative './app'
 
 class Options
   def initialize
-    @app = App.new
+    @app = App.new(self)
     show_options
   end
 
@@ -15,83 +15,29 @@ class Options
     puts '5 - Create a rental'
     puts '6 - List all rentals for a given person id'
     puts '7 - Exit'
-    selector = gets.chomp.to_i
-    if selector == 1
-      @app.list_books
-      show_options
-    elsif selector == 2
-      @app.list_people
-      show_options
-    else
-      choice(selector)
-    end
+    selector = gets.chomp
+    choice(selector)
   end
 
   def choice(selector)
-    case selector
-    when 3
-      create_person
-    when 4
-      creating_book
-    when 5
-      creating_rental
-    when 6
-      listing_rentals
-    when 7
-      puts 'Exited'
+    options = {
+      '1' => :list_books,
+      '2' => :list_people,
+      '3' => :create_person,
+      '4' => :create_book,
+      '5' => :create_rental,
+      '6' => :list_rentals,
+      '7' => :exit
+    }
+
+    method = options[selector]
+    if method.nil?
+      puts 'Invalid option, please try again!'
+      puts
+      show_options
+    else
+      @app.send(method)
     end
-  end
-
-  def create_person
-    print 'Select [0] to create teacher or [1] to create student: '
-    type = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp
-    print 'Age: '
-    age = gets.chomp.to_i
-    if type.zero?
-      print 'Specialization: '
-      specialization = gets.chomp
-      @app.create_teacher(age, name, specialization)
-    elsif type == 1
-      print 'Has parent permission? [Y/N]: '
-      parent_permission = gets.chomp.capitalize
-      @app.create_student(age, name, true) if parent_permission == 'Y'
-      @app.create_student(age, name, false) if parent_permission == 'N'
-    end
-    puts 'Person created successfully!'
-    show_options
-  end
-
-  def creating_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    @app.create_book(title, author)
-    puts 'Book created successfully'
-    show_options
-  end
-
-  def listing_rentals
-    print 'ID of person: '
-    id = gets.chomp.to_i
-    @app.list_rentals(id)
-    show_options
-  end
-
-  def creating_rental
-    puts 'Select a book from the following list by number'
-    @app.list_books
-    book = gets.chomp.to_i
-    puts 'Select a person from the following list by number (not id)'
-    @app.list_people
-    person = gets.chomp.to_i
-    print 'Enter date (yyy/mm/dd): '
-    date = gets.chomp
-    @app.create_rental(date, book, person)
-    puts 'Rental created successfully'
-    show_options
   end
 end
 
